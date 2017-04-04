436a475c68aa84c4ce20fdfe2fcce31efbc934c7
LiNk-NY <mramos09@gmail.com>
2017-04-04 19:10:58 -0400

    update RaggedExperiment-package doc page, 0.99.4

--- R/RaggedExperiment-pkg.R
+++ R/RaggedExperiment-pkg.R
@@ -1,16 +1,20 @@
 #' RaggedExperiment: Range-based data representation package
 #'
-#' @description RaggedExperiment allows the user to represent, copy
+#' @description \link{RaggedExperiment} allows the user to represent, copy
 #'     number, mutation, and other types of range-based data formats
 #'     where optional information about samples can be provided. At
-#'     the backbone of this package is the 'GRangesList' class. The
-#'     RaggedExperiment class uses this representation and presents
-#'     the data in a couple of different ways: 1) rowRanges and 2)
-#'     colData. The rowRanges method will return the internal
-#'     GRangesList representation of the dataset. A distinction
-#'     between the GRangesList and the RaggedExperiment classes is
-#'     that the RaggedExperiment class allows for ragged ranges,
-#'     meaning that there may be a different number of ranges for each
-#'     sample.
-#' @aliases NULL
+#'     the backbone of this package is the \linkS4class{GRangesList}
+#'     class. The RaggedExperiment class uses this representation
+#'     and presents the data in a couple of different ways:
+#'     \itemize{
+#'     \item rowRanges
+#'     \item colData
+#'     }
+#'     The \link{rowRanges} method will return the internal
+#'     \code{GRangesList} representation of the dataset. A distinction
+#'     between the \code{GRangesList} and the \code{RaggedExperiment}
+#'     classes is that the \link{RaggedExperiment} class allows for
+#'     ragged ranges, meaning that there may be a different number
+#'     of ranges for each sample.
+#' @aliases RaggedExperiment-package NULL
 "_PACKAGE"
